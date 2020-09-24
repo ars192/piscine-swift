6d3d1ba6ce1a698708231c915ffe937ad14abd79
ars192 <amanzholov.arystan@gmail.com>
2020-09-24 22:01:21 +0600

    added: d00ex00

---
+++ d00/ex00/ex00/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by Arystan on 9/24/20.
+//  Copyright © 2020 Arystan Amanzholov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func clickMe(_ sender: Any) {
+        print("Hello, World!")
+    }
+}
+
